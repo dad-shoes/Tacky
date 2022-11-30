af997359b6d5efe4c3f9e713fc19f336312675a2
Joshua <joshuawilliams7800@gmail.com>
2022-11-30 06:03:02 -0600

    board logic complete, including check winner, and started score tracking but not complete yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
+const player = name => {
+  const sayHi = () => {
+    console.log(`Hi ${name}`);
+  };
+  return { sayHi };
+};
+
+const playersInfo = (() => {
+  console.log("wut");
+})();
+
 const gameBoard = (() => {
   let player = 1;
-  const allBoxes = document.querySelectorAll(".box");
+  const board = document.querySelectorAll(".box");
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  const checkWin = board => {
+    for (let [a, b, c] of winConditions) {
+      if (
+        board[a].textContent &&
+        board[a].textContent === board[b].textContent &&
+        board[b].textContent === board[c].textContent
+      ) {
+        return board[a].textContent;
+      }
+    }
+  };
+
+  const resetGameBoard = () => {
+    board.forEach(x => (x.textContent = ""));
+  };
+
   const markBoard = x => {
     if (player === 1) {
       x.textContent = "X";
@@ -9,10 +48,17 @@ const gameBoard = (() => {
       x.textContent = "O";
       player = 1;
     }
-    x.removeEventListener("click", () => markBoard(x));
-    console.log("hey");
   };
-  allBoxes.forEach(x => {
-    x.addEventListener("click", () => markBoard(x));
+
+  board.forEach(x => {
+    x.addEventListener(
+      "click",
+      () => {
+        markBoard(x);
+        let winner = checkWin(board);
+        console.log(winner);
+      },
+      { once: true }
+    );
   });
 })();
